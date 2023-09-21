83905c09ab9b49250494cb5861b559b9687efa69
Administrator <you@example.com>
2023-09-21 11:07:42 +0530

    commit

---
+++ eureka/src/test/java/com/training/eurekaserver/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.eurekaserver.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
